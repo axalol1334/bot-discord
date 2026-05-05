c3bd6ca9f8afdf18f52f95c9b4009ede808c94b8
axalol1334 <axelpasteur23@gmail.com>
2026-05-05 17:38:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,24 @@
 import discord
-from discord.ext import commands
-from flask import Flask
-from threading import Thread
+from discord.ext import commands, tasks
 import os
 
 intents = discord.Intents.default()
 intents.message_content = True
+intents.guilds = True
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
+CHANNEL_ID = 1501246653779415150  # <-- remplace par ton salon
 
 @bot.event
 async def on_ready():
     print(f"Connecté en tant que {bot.user}")
+    send_alive_message.start()  # lance la boucle
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send("Pong 🏓")
+@tasks.loop(minutes=1)
+async def send_alive_message():
+    channel = bot.get_channel(CHANNEL_ID)
+    if channel:
+        await channel.send("je fonctionne ✅")
 
-# serveur web pour uptime
-app = Flask('')
-
-@app.route('/')
-def home():
-    return "Bot actif"
-
-def run():
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 8080)))
-def keep_alive():
-    t = Thread(target=run)
-    t.start()
-
-keep_alive()
 bot.run(os.getenv("TOKEN"))
